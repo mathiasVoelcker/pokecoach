4ab30aad64ee3fd39311063d1fb5af8b7059ccc1
Mathias Voelcker <mathias_V94@hotmail.com>
2026-04-12 15:59:46 -0700

    add cors policy

--- pokecoach-api/index.js
+++ pokecoach-api/index.js
@@ -1,6 +1,9 @@
 import pokemonRoutes from './routes/pokemon.js';
 import express from 'express';
+// import 'dotenv/config';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 
 
 dotenv.config();
@@ -8,6 +11,10 @@ dotenv.config();
 const app = express();
 const port = 8080;
 
+console.log(process.env.ALLOWED_ORIGIN)
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGIN,
+}));
 app.use(express.json());
 app.use('/pokemon', pokemonRoutes);
 
